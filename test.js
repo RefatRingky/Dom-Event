a6eabbe426bdb06c21e04dce0f932f312110ca8b
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-10 19:13:43 +0600

    second commit

--- test.js
+++ test.js
@@ -1,14 +1,28 @@
-const allButton = document.getElementsByClassName('button-test');
-console.log(allButton);
+// const allButton = document.getElementsByClassName('button-test');
+// console.log(allButton);
 
-for(const button of allButton){
-    button.addEventListener('click',function (e) {
-        // console.log(e);
-        if (e.target.id == "prime") {
-            document.getElementById('title').style.color ='red';
-        }
-        else{
-            document.getElementById('title').style.color ='green';
-        }
-    })
-}
+// for(const button of allButton){
+//     button.addEventListener('click',function (e) {
+//         // console.log(e);
+//         if (e.target.id == "prime") {
+//             document.getElementById('title').style.color ='red';
+//         }
+//         else{
+//             document.getElementById('title').style.color ='green';
+//         }
+//     })
+// }
+
+
+// add addEvent listener
+let count =0;
+document.getElementById('add').addEventListener('click',function (e) {
+    count = count+1;
+    document.getElementById('count').innerText = count;
+})
+// minus addEvent listener
+
+document.getElementById('minus').addEventListener('click',function (e) {
+    count = count-1;
+    document.getElementById('count').innerText = count;
+})
